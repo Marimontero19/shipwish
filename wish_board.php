8f3d9429fd1930d02fe6b9640df03a32accb1329
Maria Jose Montero <marimontero_19@hotmail.com>
2015-05-23 15:23:07 +0200

    Sincronizacion nueva
    Datos que no se habían sincronizado correctamente

---
+++ wish_board.php
@@ -0,0 +1,54 @@
+<?php
+
+$page = 'home'; //Por ejemplo 'home', 'userprofile', etc
+
+include('includes/header.php');
+
+?>
+
+<div class="main-container">
+
+	<div id="user-search">
+	</div>
+
+	<div id="filters">
+	</div>
+
+	<div id="trip-boards">
+
+	</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+include('includes/footer.php');
+
+?>
