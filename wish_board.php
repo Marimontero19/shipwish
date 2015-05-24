527e29360ad3e8dc34e8da006598f66b104138f2
Maria Jose Montero <marimontero_19@hotmail.com>
2015-05-24 23:50:43 +0200

    proceso_wishboard3

--- wish_board.php
+++ wish_board.php
@@ -16,11 +16,11 @@ include('includes/header.php');
 		</div>
 
 		<div id="user-search" class="grid_12_1200 grid_10_1024 grid_8_768 grid_4_320 ">
-			 <form class="" method="" action="">
+			 <form method="" action="">
 		          <input type="text" name="from" value="">
 		          <input type="text" name="to" value="">
 
-		          <input type="submit" name="search" class="button search" value="Search">
+		          <input type="submit" name="search" class="button search" value="Search" class="button">
 
 		           <a href="create_trip.php" alt="Create Trip" class="button">
 		         	Create Wish
@@ -28,7 +28,9 @@ include('includes/header.php');
 	        </form>
 		</div>
 
+<hr>
 		<div id="filters" class="grid_12_1200 grid_10_1024 grid_8_768 grid_6_320">
+
 
 
 				<select id="filter_date" class="">
@@ -69,57 +71,325 @@ include('includes/header.php');
 
 	<div id="trip-boards" class="grid_10_1200 grid_10_1024 grid_8_768 grid_4_320 push_1_1200 push_1_1024 push_1_768 ">
 
+	<!--Primer wish-->
+		<div id="wisher_card" class="grid_5_1200 grid_5_1024">
+			
+			<div style="padding: 3% 3% 3% 3%">
+				<div id="basic_info_home">
+					<ul>
+						<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
+						<li><h3 class="">Pecorino Romano Cheese</h3></li>
+						<li><p>*3</p></li>
+					</ul>					
+				</div>
 
-		<div id="wisher_card" class="grid_6_1200 grid_5_1024 grid_4_768 grid_2_320">
+				<div class="display_inline_block">
 
-		</div>
+					<div id="destination_home">
+						<p>From:</p><h4>Italy</h4>
+						<br>
+						<p>To:</p><h4>Dublin</h4>
+					</div>
 
-		<div id="wisher_card" class="grid_6_1200 grid_5_1024">
-
-		</div>
-		
-	</div>
-
-		
-<!--
-			<div id="wisher_info_home">
-				<ul>
-					<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
-					<li><h3 class="" style="float:right">Pecorino Romano Cheese</h3></li>
-					<li>3</li>
-				</ul>
-
-			</div>
-
-			<section id="wish_details">
-				<ul class="">
-					<li>From: Italy</li>
-					<li>To: Dublin</li>
-				</ul>
-				<ul class="" style="float:right">
-					<li>Date of delivery: June 7th, 2015</li>
-					<li>
-						<ul class="details-wish">
+					<div id="details_home">
+						<p>Delivery:</p><h4>June 7th, 2015</h4>
+						<ul>
 							<li></li>
 							<li></li>
 							<li></li>
 						</ul>
-					</li>
-				</ul>
+					</div>
+				</div>	
+			</div>	
 
-			</section>
-
-			<section class="button look">
-				<a href="user_profile.php">Let's take a look</a>
-			</section>
+			<div class="button_look">
+				<a href="product_profile.php"><h4>Let's take a look</h4></a>
+			</div>
 
 		</div>
 
-	</div>
--->
+	<!--Segundo wish-->
 
+		<div id="wisher_card" class="grid_5_1200 grid_5_1024">
+			
+			<div style="padding: 3% 3% 3% 3%">
+				<div id="basic_info_home">
+					<ul>
+						<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
+						<li><h3 class="">Pecorino Romano Cheese</h3></li>
+						<li><p>*3</p></li>
+					</ul>					
+				</div>
+
+				<div class="display_inline_block">
+
+					<div id="destination_home">
+						<p>From:</p><h4>Italy</h4>
+						<br>
+						<p>To:</p><h4>Dublin</h4>
+					</div>
+
+					<div id="details_home">
+						<p>Delivery:</p><h4>June 7th, 2015</h4>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+				</div>	
+			</div>	
+
+			<div class="button_look">
+				<a href="product_profile.php"><h4>Let's take a look</h4></a>
+			</div>
+
+		</div>
+
+		<!--Tercer wish-->
+		<div id="wisher_card" class="grid_5_1200 grid_5_1024">
+			
+			<div style="padding: 3% 3% 3% 3%">
+				<div id="basic_info_home">
+					<ul>
+						<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
+						<li><h3 class="">Pecorino Romano Cheese</h3></li>
+						<li><p>*3</p></li>
+					</ul>					
+				</div>
+
+				<div class="display_inline_block">
+
+					<div id="destination_home">
+						<p>From:</p><h4>Italy</h4>
+						<br>
+						<p>To:</p><h4>Dublin</h4>
+					</div>
+
+					<div id="details_home">
+						<p>Delivery:</p><h4>June 7th, 2015</h4>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+				</div>	
+			</div>	
+
+			<div class="button_look">
+				<a href="product_profile.php"><h4>Let's take a look</h4></a>
+			</div>
+
+		</div>
+
+	<!--Cuarto wish-->
+
+		<div id="wisher_card" class="grid_5_1200 grid_5_1024">
+			
+			<div style="padding: 3% 3% 3% 3%">
+				<div id="basic_info_home">
+					<ul>
+						<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
+						<li><h3 class="">Pecorino Romano Cheese</h3></li>
+						<li><p>*3</p></li>
+					</ul>					
+				</div>
+
+				<div class="display_inline_block">
+
+					<div id="destination_home">
+						<p>From:</p><h4>Italy</h4>
+						<br>
+						<p>To:</p><h4>Dublin</h4>
+					</div>
+
+					<div id="details_home">
+						<p>Delivery:</p><h4>June 7th, 2015</h4>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+				</div>	
+			</div>	
+
+			<div class="button_look">
+				<a href="product_profile.php"><h4>Let's take a look</h4></a>
+			</div>
+
+		</div>
+
+
+		<!--Quinto wish-->
+		<div id="wisher_card" class="grid_5_1200 grid_5_1024">
+			
+			<div style="padding: 3% 3% 3% 3%">
+				<div id="basic_info_home">
+					<ul>
+						<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
+						<li><h3 class="">Pecorino Romano Cheese</h3></li>
+						<li><p>*3</p></li>
+					</ul>					
+				</div>
+
+				<div class="display_inline_block">
+
+					<div id="destination_home">
+						<p>From:</p><h4>Italy</h4>
+						<br>
+						<p>To:</p><h4>Dublin</h4>
+					</div>
+
+					<div id="details_home">
+						<p>Delivery:</p><h4>June 7th, 2015</h4>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+				</div>	
+			</div>	
+
+			<div class="button_look">
+				<a href="product_profile.php"><h4>Let's take a look</h4></a>
+			</div>
+
+		</div>
+
+	<!--Sexto wish-->
+
+		<div id="wisher_card" class="grid_5_1200 grid_5_1024">
+			
+			<div style="padding: 3% 3% 3% 3%">
+				<div id="basic_info_home">
+					<ul>
+						<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
+						<li><h3 class="">Pecorino Romano Cheese</h3></li>
+						<li><p>*3</p></li>
+					</ul>					
+				</div>
+
+				<div class="display_inline_block">
+
+					<div id="destination_home">
+						<p>From:</p><h4>Italy</h4>
+						<br>
+						<p>To:</p><h4>Dublin</h4>
+					</div>
+
+					<div id="details_home">
+						<p>Delivery:</p><h4>June 7th, 2015</h4>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+				</div>	
+			</div>	
+
+			<div class="button_look">
+				<a href="product_profile.php"><h4>Let's take a look</h4></a>
+			</div>
+
+		</div>
+
+		<!--Setimo wish-->
+		<div id="wisher_card" class="grid_5_1200 grid_5_1024">
+			
+			<div style="padding: 3% 3% 3% 3%">
+				<div id="basic_info_home">
+					<ul>
+						<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
+						<li><h3 class="">Pecorino Romano Cheese</h3></li>
+						<li><p>*3</p></li>
+					</ul>					
+				</div>
+
+				<div class="display_inline_block">
+
+					<div id="destination_home">
+						<p>From:</p><h4>Italy</h4>
+						<br>
+						<p>To:</p><h4>Dublin</h4>
+					</div>
+
+					<div id="details_home">
+						<p>Delivery:</p><h4>June 7th, 2015</h4>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+				</div>	
+			</div>	
+
+			<div class="button_look">
+				<a href="product_profile.php"><h4>Let's take a look</h4></a>
+			</div>
+
+		</div>
+
+	<!--Octavo wish-->
+
+		<div id="wisher_card" class="grid_5_1200 grid_5_1024">
+			
+			<div style="padding: 3% 3% 3% 3%">
+				<div id="basic_info_home">
+					<ul>
+						<li><a href="user_profile.php"><img class="user-pic" src="img/users_pic/user_1_xs.jpg" alt="user_1"></a></li>
+						<li><h3 class="">Pecorino Romano Cheese</h3></li>
+						<li><p>*3</p></li>
+					</ul>					
+				</div>
+
+				<div class="display_inline_block">
+
+					<div id="destination_home">
+						<p>From:</p><h4>Italy</h4>
+						<br>
+						<p>To:</p><h4>Dublin</h4>
+					</div>
+
+					<div id="details_home">
+						<p>Delivery:</p><h4>June 7th, 2015</h4>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+				</div>	
+			</div>	
+
+			<div class="button_look">
+				<a href="product_profile.php"><h4>Let's take a look</h4></a>
+			</div>
+
+		</div>
+
+
+
+	</div>
 
 </div>
+
+<?php
+
+$page = 'home'; //Por ejemplo 'home', 'userprofile', etc
+
+include('includes/footer.php');
+
+?>
+
+
+	
+
+
 
 
 
