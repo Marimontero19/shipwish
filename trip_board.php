3e3f99a97bc5261f297848cf6587c65270bc4599
Maria Jose Montero <marimontero_19@hotmail.com>
2015-06-24 23:42:14 +0200

    cambios 3
    Implementación de diseño en wish_board y trip_board. Hice el cambio de
    estructura que se implemente en el wish en el trips.

--- trip_board.php
+++ trip_board.php
@@ -1,72 +1,72 @@
 <?php
 
-$page = 'trip_board'; //Por ejemplo 'home', 'userprofile', etc
+$page = 'wish_board'; //Por ejemplo 'home', 'userprofile', etc
 
 include('includes/header.php');
 
 ?>
+<!--
+<hr class="full-width-line">
+<hr class="full-width-line2">
+-->
+        <div id="header_wishboard">
+            <div class="main-container">
+                <div id="wish_trips">
+                    <a href="#" alt="Wish Board">Wish Board</a> |
+                    <a href="trip_board.php" alt="Trips Board">Trips Board</a>
+                </div>
+
+                <div id="user-search">
+                     <form method="" action="">
+                          <input type="text" name="from" placeholder="From" value="" class="input-trips">
+                          <input type="text" name="to" placeholder="To" value="" class="input-trips">
+
+                          <input type="submit" name="search" class="button search" value="Search" class="button">
+
+                           <button class="outline"><a href="create_trip.php" alt="Create Trip">Create Wish</a></button>
+                    </form>
+                </div>
+
+                <div id="filters">
+
+                        <select id="filter_date" class="">
+                            <label><input type="text" class="datepicker"></label>    
+                        </select> 
 
 
-<div class="main-container">
-	<div class="container_12 container_10 container_8 container_6">
+                        <select id="filter_category" class="">
+                            <option value="*">All</option>
+                            <option value=".food/beverage">Food/Beverage</option>
+                            <option value=".electronics">Electronics</option>
+                            <option value=".fashion">Fashion</option>
+                            <option value=".home/garden">Home/Garden</option>        
+                        </select> 
 
-		<div id="header_wishboard" class="grid_10_1200 grid_10_1024 grid_8_768 grid_6_320 push_1_1200 ">
+                        <select id="filter_size" class="">
+                            <option value="*">All</option>
+                            <option value=".xs">XS</option>
+                            <option value=".s">S</option>
+                            <option value=".m">M</option>
+                            <option value=".l">L</option>  
+                            <option value=".xl">XL</option> 
+                        </select> 
 
-			<div id="wish_trips">
-				<a href="wish_board.php" alt="Wish Board">Wish Board</a> |
-				<a href="#" alt="Trips Board">Trips Board</a>
-			</div>
-
-			<div id="user-search">
-				 <form method="" action="">
-			          <input type="text" name="from" placeholder="From" value="" class="input-trips">
-			          <input type="text" name="to" placeholder="To" value="" class="input-trips">
-
-			          <input type="submit" name="search" class="button search" value="Search" class="button">
-
-			           <button class="outline"><a href="create_trip.php" alt="Create Trip">Add Trip</a></button>
-		        </form>
-			</div>
-
-
-
-			<div id="filters">
-
-					<select id="filter_date" class="">
-						<label><input type="text" class="datepicker"></label>     
-			    	</select> 
-
-			
-					<select id="filter_category" class="">
-			        	<option value="*">All</option>
-			        	<option value=".food/beverage">Food/Beverage</option>
-			        	<option value=".electronics">Electronics</option>
-			        	<option value=".fashion">Fashion</option>
-			        	<option value=".home/garden">Home/Garden</option>        
-			    	</select> 
-
-			    	<select id="filter_size" class="">
-			        	<option value="*">All</option>
-			        	<option value=".xs">XS</option>
-			        	<option value=".s">S</option>
-			        	<option value=".m">M</option>
-			        	<option value=".l">L</option>  
-			        	<option value=".xl">XL</option> 
-			    	</select> 
-
-			    	<select id="filter_reward" class="">
-			        	<option value="*">All</option>
-			        	<option value=".money">Money</option>
-			        	<option value=".airporttransfer">Airport Transfer</option>
-			        	<option value=".touristday">Tourist Day</option>
-			        	<option value=".gift">Gift</option>  
-			        	<option value=".meal">Meal</option>
-			    	</select> 
+                        <select id="filter_reward" class="">
+                            <option value="*">All</option>
+                            <option value=".money">Money</option>
+                            <option value=".airporttransfer">Airport Transfer</option>
+                            <option value=".touristday">Tourist Day</option>
+                            <option value=".gift">Gift</option>  
+                            <option value=".meal">Meal</option>
+                        </select> 
 
 
-		          <input class="" type="submit" name="search" class="button search" value="Clear filters">
-			</div>
-		</div>
+                      <input class="" type="submit" name="search" class="button search" value="Clear filters">
+                </div>
+        </div>
+
+ <div class="main-container">
+    <div class="container_12 container_10 container_8 container_6">
 
 		<div id="trip-description" class="grid_8_1200 grid_8_1024 grid_6_768 grid_6_320 push_2_1200 push_1_1024">
 				<img src="img/icons/trip_board.svg" alt="trips">
