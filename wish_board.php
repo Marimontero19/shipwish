9f677fd2413f1d1ac8ad63dc472d050003b81c16
Maria Jose Montero <marimontero_19@hotmail.com>
2015-06-28 18:50:39 +0200

    Fixing wish_board

--- wish_board.php
+++ wish_board.php
@@ -1,6 +1,6 @@
 <?php
 
-$page = 'wish_board'; //Por ejemplo 'home', 'userprofile', etc
+$page = '| Wishes'; //Por ejemplo 'home', 'userprofile', etc
 
 include('includes/header.php');
 
@@ -10,62 +10,85 @@ include('includes/header.php');
 <hr class="full-width-line2">
 -->
         <div id="header_wishboard">
-            <div class="main-container">
+            <div class="main-container" >
                 <div id="wish_trips">
-                    <a href="#" alt="Wish Board">Wish Board</a> |
-                    <a href="trip_board.php" alt="Trips Board">Trips Board</a>
+                    <ul>
+                         <li>
+                             <button><img src="img/icons/more2.svg" alt="more">
+                            <a href="create_wish.php" alt="Create Trip">New wish</a></button>
+                        </li>
+
+                        <li>
+                            <img src="img/icons/wishes.svg" alt="Wishes">
+                            <a href="#" alt="Wish Board">Wishes</a>
+                        </li>
+
+                          <li>
+                            <img src="img/icons/trips.svg" alt="trips">
+                            <a href="trip_board.php" alt="Trips Board">Trips</a>
+                        </li>
                 </div>
-
-                <div id="user-search">
-                     <form method="" action="">
-                          <input type="text" name="from" placeholder="From" value="" class="input-trips">
-                          <input type="text" name="to" placeholder="To" value="" class="input-trips">
-
-                          <input type="submit" name="search" class="button search" value="Search" class="button">
-
-                           <button class="outline"><a href="create_trip.php" alt="Create Trip">Create Wish</a></button>
-                    </form>
+                <div class="triangle-down">
                 </div>
-
-                <div id="filters">
-
-                        <select id="filter_date" class="">
-                            <label><input type="text" class="datepicker"></label>    
-                        </select> 
-
-
-                        <select id="filter_category" class="">
-                            <option value="*">All</option>
-                            <option value=".food/beverage">Food/Beverage</option>
-                            <option value=".electronics">Electronics</option>
-                            <option value=".fashion">Fashion</option>
-                            <option value=".home/garden">Home/Garden</option>        
-                        </select> 
-
-                        <select id="filter_size" class="">
-                            <option value="*">All</option>
-                            <option value=".xs">XS</option>
-                            <option value=".s">S</option>
-                            <option value=".m">M</option>
-                            <option value=".l">L</option>  
-                            <option value=".xl">XL</option> 
-                        </select> 
-
-                        <select id="filter_reward" class="">
-                            <option value="*">All</option>
-                            <option value=".money">Money</option>
-                            <option value=".airporttransfer">Airport Transfer</option>
-                            <option value=".touristday">Tourist Day</option>
-                            <option value=".gift">Gift</option>  
-                            <option value=".meal">Meal</option>
-                        </select> 
-
-
-                      <input class="" type="submit" name="search" class="button search" value="Clear filters">
-                </div>
+            </div>
         </div>
 
-<div class="main-container">
+        <div id="header-search">
+            <div class="main-container">
+                <div class="grid_10_1200 grid_10_1024 grid_8_768 grid_6_320 push_1_1200">
+                     <div id="user-search" >
+                        <h4>What are you willing to bring?</h4>
+                         <form method="" action="">
+                              <input type="text" name="from" placeholder="From" value="" class="input-trips">
+                              <img src="img/icons/estela.svg" alt="Shipwish">
+                              <input type="text" name="to" placeholder="To" value="" class="input-trips">
+
+                              <input type="submit" name="search" value="Search">
+                        </form>
+                    </div>
+
+                    <!--<div id="filters">
+
+                            <select id="filter_date" class="">
+                                <label><input type="text" class="datepicker"></label>    
+                            </select> 
+
+
+                            <select id="filter_category" class="">
+                                <option value="*">All</option>
+                                <option value=".food/beverage">Food/Beverage</option>
+                                <option value=".electronics">Electronics</option>
+                                <option value=".fashion">Fashion</option>
+                                <option value=".home/garden">Home/Garden</option>        
+                            </select> 
+
+                            <select id="filter_size" class="">
+                                <option value="*">All</option>
+                                <option value=".xs">XS</option>
+                                <option value=".s">S</option>
+                                <option value=".m">M</option>
+                                <option value=".l">L</option>  
+                                <option value=".xl">XL</option> 
+                            </select> 
+
+                            <select id="filter_reward" class="">
+                                <option value="*">All</option>
+                                <option value=".money">Money</option>
+                                <option value=".airporttransfer">Airport Transfer</option>
+                                <option value=".touristday">Tourist Day</option>
+                                <option value=".gift">Gift</option>  
+                                <option value=".meal">Meal</option>
+                            </select> 
+
+
+                          <input class="" type="submit" name="search" class="button search" value="Clear filters">
+                    </div>-->
+                </div>
+                <div class="clear"></div>
+        </div>
+    </div>
+
+<div class="main-container" style="background-color:#f9f9f9">
     <div class="container_12 container_10 container_8 container_6">
 
         <div id="wish-description" class="grid_8_1200 grid_8_1024 grid_6_768 grid_6_320 push_2_1200 push_1_1024">
