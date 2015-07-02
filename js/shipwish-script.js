19dd663896d1b21b0d0e97b052e1e0c0fa0965ea
Maria Jose Montero <marimontero_19@hotmail.com>
2015-07-02 20:01:57 +0200

    cambios en script camilo

--- js/shipwish-script.js
+++ js/shipwish-script.js
@@ -18,17 +18,42 @@ $(document).ready (
 		 $(function() {
 		    $( "#radio" ).buttonset();
 		  });
-/*
+
 $(document).ready(function(){
-    $('.card').hover(function(){
-        $('.button_look').animation({
+    $('.card').on("mouseenter",function(){
+
+        var ref=$(this);
+        $(this).find('.button_look').animate({
             height: "36px"
         }, 100, function(){
-            $('.button_look h4').css("display","block");
+           // ref.find('.button_look h4').css("display","block");
         });
+        $(this).find('.button_look h4').css("display","block");
+/*
+ $(this).find('.button_look').css('height','36px');
+$(this).find('.button_look h4').css("display","block");
+*/
     });
 });
+
+
+$(document).ready(function(){
+    $('.card').on("mouseleave",function(){
+
+        var ref=$(this);
+        $(this).find('.button_look').animate({
+            height: "4px"
+        }, 100, function(){
+           // ref.find('.button_look h4').css("display","block");
+        });
+        $(this).find('.button_look h4').css("display","none");
+/*
+ $(this).find('.button_look').css('height','36px');
+$(this).find('.button_look h4').css("display","block");
 */
+    });
+});
+
 
 /*
 //HEIGHT
